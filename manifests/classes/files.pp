585e0b9a549eba365084eba1e05ea90b2ec3af9e
dwohlfahrt <dwohlfahrt@gmail.com>
2014-01-14 12:44:10 -0600

    changing to use v. 1.4 of tika jar

--- manifests/classes/files.pp
+++ manifests/classes/files.pp
@@ -1,15 +1,15 @@
 class tika::files {
   exec { "download_tika":
     cwd => "/tmp",
-    command => "/usr/bin/wget -c http://archive.apache.org/dist/tika/tika-app-0.10.jar",
-    creates => "/tmp/tika-app-0.10.jar",
+    command => "/usr/bin/wget -c http://www.apache.org/dyn/closer.cgi/tika/tika-app-1.4.jar",
+    creates => "/tmp/tika-app-1.4.jar",
     timeout => 0,
   }
 
   exec { "deploy_tika":
     cwd => "/tmp",
-    command => "/bin/mv /tmp/tika-app-0.10.jar /opt/tika/tika-app-0.10.jar",
-    creates => "/opt/tika/tika-app-0.10.jar",
+    command => "/bin/mv /tmp/tika-app-1.4.jar /opt/tika/tika-app-1.4.jar",
+    creates => "/opt/tika/tika-app-1.4.jar",
     require => Exec["download_tika"],
   }
 }
